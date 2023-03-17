cd47529a4c011d25ef85f92ad1fe328137f53046
Nemurs <kevinoctaviomejia@gmail.com>
2023-03-17 15:22:03 -0700

    added backend endoint for restoring user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -45,12 +45,30 @@ router.post(
     }
   );
 
-  // Log out
+// Log out
 router.delete(
     '/',
     (_req, res) => {
       res.clearCookie('token');
       return res.json({ message: 'success' });
+    }
+  );
+
+// Restore session user
+router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
     }
   );
 
