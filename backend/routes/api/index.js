656a763ac6ac399bb21cd0b32e2c45642a247354
Nemurs <kevinoctaviomejia@gmail.com>
2023-03-17 15:10:37 -0700

    added user login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,8 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
-router.use(restoreUser);
 
 // Add a XSRF-TOKEN cookie
 router.get("/csrf/restore", (req, res) => {
@@ -11,5 +12,19 @@ router.get("/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+router.use(restoreUser);
+
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
