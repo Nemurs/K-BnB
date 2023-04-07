7fe9b08b348647f8bff09024c052db3fe6b52e7d
Nemurs <kevinoctaviomejia@gmail.com>
2023-04-07 14:51:46 -0700

    redux actions and reducer for logging in and out

---
+++ frontend/src/store/session.js
@@ -0,0 +1,60 @@
+// frontend/src/store/session.js
+import { csrfFetch } from "./csrf";
+
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
+
+const setUserAction = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+const removeUserAction = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+export const loginThunk = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUserAction(data.user));
+  return response;
+};
+
+export const logoutThunk = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session", {
+      method: "DELETE"
+    });
+    dispatch(removeUserAction());
+    return response;
+  };
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.payload;
+      return newState;
+    case REMOVE_USER:
+      newState = Object.assign({}, state);
+      newState.user = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
