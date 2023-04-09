7d0728a08c84a57d45eedfcaa09fda7b8aff60d8
Nemurs <kevinoctaviomejia@gmail.com>
2023-04-09 12:07:59 -0700

    fixed modal context and added login modal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,15 @@ import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
-import { csrfFetch } from './store/csrf';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from "./store/session";
-import { ModalProvider } from './context/Modal';
+import { ModalProvider, Modal } from './context/Modal';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
@@ -23,6 +25,7 @@ function Root() {
       <ReduxProvider store={store}>
         <BrowserRouter>
           <App />
+          <Modal />
         </BrowserRouter>
       </ReduxProvider>
     </ModalProvider>
