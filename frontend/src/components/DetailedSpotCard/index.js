a85828b74aed97620297ad2488f61410f6e69c3c
Nemurs <kevinoctaviomejia@gmail.com>
2023-04-10 23:05:17 -0700

    load single spot into store

--- frontend/src/components/DetailedSpotCard/index.js
+++ frontend/src/components/DetailedSpotCard/index.js
@@ -1,8 +1,20 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadOneThunk } from "../../store/singleSpot";
 
 const DetailedSpotCard = () => {
+    const dispatch = useDispatch();
     const {id} = useParams();
+    const spot = useSelector(state => Object.values(state.spots.singleSpot));
+
+
+    useEffect(() => {
+        dispatch(loadOneThunk(id));
+    }, [dispatch]);
+
     console.log(id)
+    console.log(spot)
     return (
         <h1>{`Spot: ${id}`}</h1>
     )
--- frontend/src/components/SpotBrowser/index.js
+++ frontend/src/components/SpotBrowser/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SpotCard from '../SpotCard';
 import { loadAllThunk } from '../../store/allSpots';
 import "./SpotBrowser.css";
