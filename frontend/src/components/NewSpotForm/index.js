5aa1a21dc680dc28060cc34f12c1111830e016a5
Nemurs <kevinoctaviomejia@gmail.com>
2023-04-12 17:40:37 -0700

    finished client-side form validation

--- frontend/src/components/NewSpotForm/index.js
+++ frontend/src/components/NewSpotForm/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createNewSpotImageThunk, createNewSpotThunk, loadAllThunk } from '../../store/allSpots';
 import "./NewSpotForm.css";
@@ -22,9 +22,14 @@ const NewSpotForm = () => {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
     const [previewImageURL, setPreviewImageURL] = useState('');
+    const [error, setError] = useState({});
+    const [touched, setTouched] = useState({});
+    const [submitState, setSubmitState] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setSubmitState(true);
+
         const newSpot = {
             country,
             address,
@@ -37,23 +42,31 @@ const NewSpotForm = () => {
             description,
         };
 
+        const errorMsgs = Object.values(error);
+
+        if (errorMsgs.length >= 1) {
+            let alertMsg = errorMsgs.reduce((acc, val) => acc + `    ${val}\n`, "")
+            alert(`Please fix the following errors before submitting: \n${alertMsg}`);
+            return;
+        };
+
         let spotRes = await dispatch(createNewSpotThunk(newSpot));
-        if(spotRes.ok){
+        if (spotRes.ok) {
             let spotData = await spotRes.json();
             console.log(spotData);
-            let imgPayload = {id: spotData.id, body:{url: previewImageURL, preview: true}}
+            let imgPayload = { id: spotData.id, body: { url: previewImageURL, preview: true } }
             let imgRes = await dispatch(createNewSpotImageThunk(imgPayload));
-            if(imgRes.ok){
+            if (imgRes.ok) {
                 history.push(`/spots/${spotData.id}`);
                 reset();
             }
-            else{
+            else {
                 console.log("error with img data")
                 console.log(imgRes);
                 console.log(spotData);
             }
         }
-        else{
+        else {
             console.log("error with spot data")
             console.log(spotRes);
         }
@@ -72,6 +85,36 @@ const NewSpotForm = () => {
         setPreviewImageURL('');
     };
 
+    useEffect(() => {
+        let newErrors = {}
+        //Special Errors
+        if (name.length < 2 || name.length > 255) newErrors.name = "Name must be between 2 and 255 characters";
+
+        if (!price.length || isNaN(Number(price)) || Number(price) < 0) newErrors.price = "Price must be a number greater than 0";
+
+        if (isNaN(Number(latitude)) || Number(latitude) < -90 || Number(latitude) > 90) newErrors.latitude = "Latitude must be a number between -90 and 90";
+
+        if (isNaN(Number(longitude)) || Number(longitude) < -180 || Number(longitude) > 180) newErrors.longitude = "Longitude must be a number between -90 and 90";
+
+        if (description.length < 30 || description.length > 255) newErrors.description = "Description must be between 30 and 255 characters"
+
+        if (!(previewImageURL.endsWith(".png") || previewImageURL.endsWith(".jpg") || previewImageURL.endsWith(".jpeg") || previewImageURL.length)) {
+            newErrors.previewImageURL = "Image URL must end in .png, .jpg, or .jpeg"
+        }
+
+        //Missing Required Field Errors
+        if (!country.length) newErrors.country = "Country is Required";
+        if (!address.length) newErrors.address = "Address is Required";
+        if (!city.length) newErrors.city = "City is Required";
+        if (!state.length) newErrors.state = "State is Required";
+
+        setError(newErrors)
+
+        return () => setError({});
+
+
+    }, [country, address, city, state, latitude, longitude, description, name, price, previewImageURL]);
+
     return (
         <div className='create-new-spot-page'>
             <h1>Create a new Spot</h1>
@@ -89,6 +132,7 @@ const NewSpotForm = () => {
                             placeholder='Country'
                             name='country'
                         />
+                        {(submitState && error.country) && <p className="form-error">{error.country}</p>}
                         <label>Street Address</label>
                         <input
                             type='text'
@@ -96,7 +140,9 @@ const NewSpotForm = () => {
                             value={address}
                             placeholder='Address'
                             name='address'
+                            required={true}
                         />
+                        {(submitState && error.address) && <p className="form-error">{error.address}</p>}
                         <label>City</label>
                         <input
                             type='text'
@@ -104,7 +150,9 @@ const NewSpotForm = () => {
                             value={city}
                             placeholder='City'
                             name='city'
+                            required={true}
                         />
+                        {(submitState && error.city) && <p className="form-error">{error.city}</p>}
                         <label>State</label>
                         <input
                             type='text'
@@ -112,7 +160,9 @@ const NewSpotForm = () => {
                             value={state}
                             placeholder='STATE'
                             name='state'
+                            required={true}
                         />
+                        {(submitState && error.state) && <p className="form-error">{error.state}</p>}
                         <label>Latitude</label>
                         <input
                             type='text'
@@ -120,7 +170,9 @@ const NewSpotForm = () => {
                             value={latitude}
                             placeholder='Latitude'
                             name='latitude'
+                            onBlur={() => setTouched({ ...touched, 'latitude': true })}
                         />
+                        {(touched.latitude && error.latitude) && <p className="form-error">{error.latitude}</p>}
                         <label>Longitude</label>
                         <input
                             type='text'
@@ -128,20 +180,25 @@ const NewSpotForm = () => {
                             value={longitude}
                             placeholder='Longitude'
                             name='longitude'
+                            onBlur={() => setTouched({ ...touched, 'longitude': true })}
                         />
-
+                        {(touched.longitude && error.longitude) && <p className="form-error">{error.longitude}</p>}
                     </div>
                     <div className='create-new-spot-form-description'>
                         <h3>Describe your place to guests</h3>
                         <p>Mention the best features of your space, any special amentities like fast wifi or parking, and what you love about the neighborhood.</p>
                         <label>Description</label>
+                        <br />
                         <textarea
                             value={description}
                             onChange={(e) => setDescription(e.target.value)}
                             name='description'
                             placeholder='Please write at least 30 characters'
                             rows='10'
+                            required={true}
+                            onBlur={() => setTouched({ ...touched, 'description': true })}
                         ></textarea>
+                        {(touched.description && error.description) && <p className="form-error">{error.description}</p>}
                     </div>
                     <div className='create-new-spot-form-name'>
                         <h3>Create a title for your spot</h3>
@@ -152,18 +209,24 @@ const NewSpotForm = () => {
                             value={name}
                             placeholder='Name of your spot'
                             name='name'
+                            required={true}
+                            onBlur={() => setTouched({ ...touched, 'name': true })}
                         />
+                        {(touched.name && error.name) && <p className="form-error">{error.name}</p>}
                     </div>
                     <div className='create-new-spot-form-price'>
                         <h3>Set a base price for your spot</h3>
-                        <p>Competitive pricing can help your listing stand out and rank higher in search results.</p>
+                        <p>Competitive pricing can help your listing stand out and rank higher in search results. </p>
                         <input
                             type='text'
                             onChange={(e) => setPrice(e.target.value)}
                             value={price}
                             placeholder='Price per night (USD)'
                             name='price'
+                            required={true}
+                            onBlur={() => setTouched({ ...touched, 'price': true })}
                         />
+                        {(touched.price && error.price) && <p className="form-error">{error.price}</p>}
                     </div>
                     <div className='create-new-spot-form-image'>
                         <h3>Liven up your spot with photos</h3>
@@ -174,7 +237,10 @@ const NewSpotForm = () => {
                             value={previewImageURL}
                             placeholder='Preview Image URL'
                             name='previewImageURL'
+                            required={true}
+                            onBlur={() => setTouched({ ...touched, 'previewImageURL': true })}
                         />
+                        {(touched.previewImageURL && error.previewImageURL) && <p className="form-error">{error.previewImageURL}</p>}
                     </div>
                     <button type='submit'>Create Spot</button>
                 </form>
