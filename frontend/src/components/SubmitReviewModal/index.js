d96755e9828c4c83afb69bf4658b22869962f2f7
Nemurs <kevinoctaviomejia@gmail.com>
2023-04-14 15:52:09 -0700

    updated modal css and added front-end validations

--- frontend/src/components/SubmitReviewModal/index.js
+++ frontend/src/components/SubmitReviewModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal.js";
 import StarRatingInput from "../StarRatingInput";
@@ -6,18 +6,23 @@ import "./SubmitReviewModal.css";
 import { createNewReviewThunk, loadAllReviewsThunk } from "../../store/singleSpotReviews.js";
 import { loadOneThunk } from "../../store/singleSpot.js";
 
-function SubmitReviewModal({spotId, user}) {
+function SubmitReviewModal({ spotId, user }) {
     const dispatch = useDispatch();
     const [reviewText, setReviewText] = useState("");
-    const [rating, setRating] = useState(5);
+    const [rating, setRating] = useState(0);
+    const [error, setError] = useState({});
+    const [disabled, setDisabled] = useState(true);
+    const [touched, setTouched] = useState({});
+    const [submitState, setSubmitState] = useState(false);
     const { closeModal } = useModal();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setSubmitState(true);
 
-        let res = await dispatch(createNewReviewThunk({spotId: Number(spotId), body:{review:reviewText, stars:rating}, user}));
+        let res = await dispatch(createNewReviewThunk({ spotId: Number(spotId), body: { review: reviewText, stars: rating }, user }));
 
-        if(!res.ok){
+        if (!res.ok) {
             console.log(await res.json());
             // console.log(spots);
         } else {
@@ -31,6 +36,30 @@ function SubmitReviewModal({spotId, user}) {
         setRating(number);
     };
 
+    useEffect(() => {
+        let newErrors = {};
+
+        if (reviewText.length < 10 || reviewText.length > 255) {
+            newErrors.review = "Review must be between 10 and 255 characters";
+            setDisabled(true);
+        }
+        else delete newErrors.review;
+
+        if (rating < 1 || rating > 5) {
+            newErrors.rating = "Rating must be between 1 and 5 stars";
+            setDisabled(true);
+        }
+        else delete newErrors.rating;
+
+        setError(newErrors)
+
+        return () => setError({});
+    }, [reviewText, rating])
+
+    useEffect(() => {
+        if (reviewText.length > 10 && rating > 0 && !error.rating && !error.review) setDisabled(false);
+    }, [error])
+
     return (
         <div className="submit-review-modal">
             <h1>How was your Stay?</h1>
@@ -41,18 +70,26 @@ function SubmitReviewModal({spotId, user}) {
                     name='reviewText'
                     placeholder='Leave your review here...'
                     rows='10'
+                    onBlur={() => setTouched({ ...touched, 'review': true })}
                 ></textarea>
-                <div className="star-rating-field">
-                    <StarRatingInput
-                        disabled={false}
-                        onChange={onChange}
-                        rating={rating}
-                    />
-                    <label >
-                        Stars
-                    </label>
+                {((touched.review || submitState) && error.review) && <p className="form-error">{error.review}</p>}
+                <div className="star-rating">
+                    <div className="star-rating-input"
+                        onBlur={() => setTouched({ ...touched, 'rating': true })}
+                    >
+                        <StarRatingInput
+                            disabled={false}
+                            onChange={onChange}
+                            rating={rating}
+                        />
+                        <label >
+                            Stars
+                        </label>
+                    </div>
+                    {((touched.rating || submitState) && error.rating) && <p className="form-error">{error.rating}</p>}
                 </div>
-                <button type="submit">Submit Your Review</button>
+
+                <button disabled={disabled} type="submit" className={disabled ? "submit-review-inactive": "submit-review-active" }>Submit Your Review</button>
             </form>
         </div>
     );
