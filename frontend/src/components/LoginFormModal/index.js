1364d25312815ba7055c0dc4434138398680cc13
Nemurs <kevinoctaviomejia@gmail.com>
2023-04-14 16:36:22 -0700

    updated styling of login modal

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,20 +1,26 @@
 // frontend/src/components/LoginFormModal/index.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal.js";
-import "./LoginForm.css";
+import "./LoginFormModal.css";
 
 function LoginFormModal() {
   const dispatch = useDispatch();
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const [clientError, setClientError] = useState({});
+  const [disabled, setDisabled] = useState(true);
+  const [touched, setTouched] = useState({});
+  const [submitState, setSubmitState] = useState(false);
   const { closeModal } = useModal();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors({});
+    setSubmitState(true);
+
     return dispatch(sessionActions.loginThunk({ credential, password }))
       .then(closeModal)
       .catch(async (res) => {
@@ -25,34 +31,58 @@ function LoginFormModal() {
       });
   };
 
+  useEffect(() => {
+    let newErrors = {};
+
+    if (credential.length < 4 || credential.length > 255) {
+      newErrors.credential = "Credential must be between 4 and 255 characters";
+      setDisabled(true);
+    }
+    else delete newErrors.credential;
+
+    if (password.length < 6 || password.length > 255) {
+      newErrors.password = "Password must be between 4 and 255 characters";
+      setDisabled(true);
+    }
+    else delete newErrors.password;
+
+    setClientError(newErrors)
+
+    return () => setClientError({});
+  }, [credential, password])
+
+  useEffect(() => {
+    if (credential.length > 4 && password.length > 6 && !clientError.credential && !clientError.password) setDisabled(false);
+}, [clientError])
+
   return (
-    <>
+    <div className="login-form-modal">
       <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username or Email
+      {errors.credential && (
+          <p className="form-error">{errors.credential}</p>
+        )}
+      <form className="login-form" onSubmit={handleSubmit}>
           <input
             type="text"
             value={credential}
+            placeholder="Username or Email"
             onChange={(e) => setCredential(e.target.value)}
+            onBlur={() => setTouched({ ...touched, 'credential': true })}
             required
           />
-        </label>
-        <label>
-          Password
+          {((touched.credential || submitState) && clientError.credential) && <p className="form-error">{clientError.credential}</p>}
           <input
             type="password"
             value={password}
+            placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
+            onBlur={() => setTouched({ ...touched, 'password': true })}
             required
           />
-        </label>
-        {errors.credential && (
-          <p>{errors.credential}</p>
-        )}
-        <button type="submit">Log In</button>
+          {((touched.password || submitState) && clientError.password) && <p className="form-error">{clientError.password}</p>}
+        <button disabled={disabled} type="submit" className={disabled ? "submit-login-inactive": "submit-login-active" }>Log In</button>
       </form>
-    </>
+    </div>
   );
 }
 
