ed7f2a27ecdc512778cd7c010bf0c9b82285eb09
Nemurs <kevinoctaviomejia@gmail.com>
2023-07-03 13:30:41 -0700

    performance update/bugfix for calendar

--- frontend/src/components/UserBookingBrowser/index.js
+++ frontend/src/components/UserBookingBrowser/index.js
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
-import {format} from "date-fns";
-import SpotCard from '../SpotCard';
+import { format, isWithinInterval } from "date-fns";
 import { loadUserOwnedThunk } from '../../store/allBookings';
 import { loadOneBookingThunk } from "../../store/singleBooking";
 import { loadOneThunk } from "../../store/singleSpot";
-import "./UserBookingBrowser.css";
+import SpotCard from '../SpotCard';
 import OpenModalButton from '../OpenModalButton';
 import DeleteBookingModal from '../DeleteBookingModal';
+import "./UserBookingBrowser.css";
 
 const UserBookingBrowser = () => {
     const dispatch = useDispatch();
@@ -41,27 +41,30 @@ const UserBookingBrowser = () => {
             {bookings.length ? (
                 <ul className='spot-list'>
                     {bookings.map((book) => {
-                    const spot = book.Spot
-                    if(!spot || book.expired) return (<></>)
-                    let startsToday = (new Date(book.startDate)).getDate() === (new Date()).getDate()
-                    return (
-                        <li key={spot["id"]} className='spot-card-list-item' style={startsToday ? {border:"2.5px solid #f9385d", borderRadius:"20px"}:{}}>
-                            <Link to={`../spots/${spot["id"]}`} className='spot-card-link'>
-                                <span>{`${format(new Date(book.startDate),"yyyy/MM/dd")} - ${format(new Date(book.endDate),"yyyy/MM/dd")}`}</span>
-                                <SpotCard spot={spot} tooltip={spot["name"]} />
-                            </Link>
-                            <div className='user-spot-buttons'>
-                                <Link to={`../reserve/${spot["id"]}`}>
-                                    <button className="user-spot-update-button" onClick={e => handleClick(e, spot["id"])}>Update</button>
+                        const spot = book.Spot
+                        if(!spot || book.expired) return (<></>)
+                        const start = new Date(book.startDate)
+                        const end = new Date(book.endDate)
+                        const isInProgress = isWithinInterval(new Date(), {start, end})
+                        return (
+                            <li key={spot["id"]} className='spot-card-list-item' style={isInProgress ? {border:"2.5px solid #f9385d", borderRadius:"20px"}:{}}>
+                                <Link to={`../spots/${spot["id"]}`} className='spot-card-link'>
+                                    <span>{`${format(start,"yyyy/MM/dd")} - ${format(end,"yyyy/MM/dd")}`}</span>
+                                    <SpotCard spot={spot} tooltip={spot["name"]} />
                                 </Link>
-                                <OpenModalButton
-                                    buttonText="Cancel Booking"
-                                    cssClass={"user-spot-delete-button"}
-                                    modalComponent={<DeleteBookingModal bookingId={book.id} />}
-                                />
-                            </div>
-                        </li>
-                    )})}
+                                <div className='user-spot-buttons'>
+                                    <Link to={`../reserve/${spot["id"]}`}>
+                                        <button className="user-spot-update-button" onClick={e => handleClick(e, spot["id"])}>Update</button>
+                                    </Link>
+                                    <OpenModalButton
+                                        buttonText="Cancel Booking"
+                                        cssClass={"user-spot-delete-button"}
+                                        modalComponent={<DeleteBookingModal bookingId={book.id} />}
+                                    />
+                                </div>
+                            </li>
+                        )
+                    })}
                 </ul>
             ):(<></>)}
         </div>
